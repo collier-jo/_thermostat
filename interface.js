25389f340a4386f03b326781119b298831bb4d67
joannacollier <collj035@gmail.com>
2020-05-14 18:40:52 +0100

    All buttons functioning via JQuery

--- interface.js
+++ interface.js
@@ -6,16 +6,47 @@ $(document).ready(function() {
     $('#up').click(function() {
       console.log("I have clicked the up button");
     });
+//////////
 
-    $('#powermode').click(function() {
-      thermostat.switchPowerSavingModeOn();
-      $('#Power_Mode').text('on');
+    $('#up').click(function() {
+      thermostat.up()
+      $('#Temperature').text(thermostat.getCurrentTemperature());
+      $('#Energy_Usage').text(thermostat.energyUsage());
     });
-    
+
     $('#down').click(function() {
       thermostat.down()
       $('#Temperature').text(thermostat.getCurrentTemperature());
+      $('#Energy_Usage').text(thermostat.energyUsage());
     });
+
+///////
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    console.log(thermostat.powerSavingMode);
+    $('#Power_Mode').text('On');
+  });
+
+/////////
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    console.log(thermostat.powerSavingMode);
+    $('#Power_Mode').text('Off');
+  });
+
+////////////
+
+  $('#reset').click(function() {
+    thermostat.resetTemperature()
+    $('#Temperature').text(thermostat.getCurrentTemperature());
+    $('#Energy_Usage').text(thermostat.energyUsage());
+  });
+
+//////
+
+
+    
 
     
 });
